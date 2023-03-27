39ba588d4d3ade15354e1c9f32f0ba9a87c965f7
EsquiloFabio <fbastos162@gmail.com>
2023-03-27 19:29:43 -0300

    exercicio

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Simples from "./componentes/Simples";
+
+export default class app extends Component{
+  render() {
+    return (
+      <View style = {estilos.container}>
+        <Simples/>
+      </View>
+    );
+    
+  };
+  
+};
+
+const estilos = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  fonte: {
+    fontSize: 50,
+  },
+});
+
